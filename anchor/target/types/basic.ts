0751798ee7f3a00f5daf3d327de0617d1d27adfb
maplerichie <10757316+maplerichie@users.noreply.github.com>
2024-08-26 22:28:59 +0800

    Fix instruction

--- anchor/target/types/basic.ts
+++ anchor/target/types/basic.ts
@@ -5,19 +5,42 @@
  * IDL can be found at `target/idl/basic.json`.
  */
 export type Basic = {
-  address: '79zUy3DKMd4xfFpjnzBVyxaaEzD6vV6aAVzHS8U7JS1Y';
-  metadata: {
-    name: 'basic';
-    version: '0.1.0';
-    spec: '0.1.0';
-    description: 'Created with Anchor';
-  };
-  instructions: [
+  "address": "79zUy3DKMd4xfFpjnzBVyxaaEzD6vV6aAVzHS8U7JS1Y",
+  "metadata": {
+    "name": "basic",
+    "version": "0.1.0",
+    "spec": "0.1.0",
+    "description": "Created with Anchor"
+  },
+  "instructions": [
     {
-      name: 'greet';
-      discriminator: [203, 194, 3, 150, 228, 58, 181, 62];
-      accounts: [];
-      args: [];
+      "name": "greet",
+      "discriminator": [
+        203,
+        194,
+        3,
+        150,
+        228,
+        58,
+        181,
+        62
+      ],
+      "accounts": [],
+      "args": []
     }
-  ];
+  ],
+  "errors": [
+    {
+      "code": 6000,
+      "name": "customError",
+      "msg": "Custom error message"
+    }
+  ],
+  "constants": [
+    {
+      "name": "seed",
+      "type": "string",
+      "value": "\"anchor\""
+    }
+  ]
 };
